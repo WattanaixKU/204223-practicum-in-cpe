90ecbacb2d94b56eadb8124f77d99aabe4b6a912
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-03-29 15:17:41 +0700

    done

---
+++ make_file/count-sw.c
@@ -0,0 +1,23 @@
+//https://www.youtube.com/watch?embed=no&v=ZvJrOgB8rQM
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+#include "peri.h"
+
+int main()
+{
+    uint8_t count = 0;
+    init_peri();
+    set_led_value(0);
+    while(1)
+    {
+        while(!SWITCH_PRESSED())
+            ;
+        _delay_ms(50);
+        count++;
+        set_led_value(count);
+        while(SWITCH_PRESSED())
+            ;
+        _delay_ms(50);
+    }
+}
---
+++ make_file/light-8.c
@@ -0,0 +1,35 @@
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+#include "peri.h"
+
+int main()
+{
+    uint8_t level;
+    uint16_t light;
+    init_peri();
+    while(1)
+    {
+        light = read_adc(PC4);
+        level = (light/128);
+        /*
+        level = 7-(light-380)/50;
+        if(light<500)
+            level = 0;
+        else if(light<600)
+            level = 1;
+        else if(light<700)
+            level = 2;
+        else if(light<740)
+            level = 3;
+        else if(light<780)
+            level = 4;
+        else if(light<820)
+            level = 5;
+        else if(light<840)
+            level = 6;
+        else
+        */
+        set_led_value(level);
+    }
+}
